c27d188d8b173a2680a694bc41f7d075f58371aa
LEOLEO204 <doanhaixuyen17@gmail.com>
2026-07-19 15:46:15 +0700

    change backend server port to 5000

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Enable wide body size limit for base64 images upload
 app.use(cors());
